c5979bbfbc91e213274a8e6c20357ec2672b281b
vnoook <noook@yandex.ru>
2022-03-11 12:16:09 +0700

    #19 continue main.py edit def do_fill_data

--- main.py
+++ main.py
@@ -179,156 +179,140 @@ class Window(PyQt5.QtWidgets.QMainWindow):
         set_data_full_file = set()
         set_data_half_file = set()
 
-        # # проверка на то что четыре "главные" комбобокса заполнены
-        # if self.text_empty_combobox not in (self.comboBox_liter_IC.itemText(self.comboBox_liter_IC.currentIndex()),
-        #                                     self.comboBox_digit_IC.itemText(self.comboBox_digit_IC.currentIndex()),
-        #                                     self.comboBox_liter_GASPS.itemText(
-        #                                         self.comboBox_liter_GASPS.currentIndex()),
-        #                                     self.comboBox_digit_GASPS.itemText(
-        #                                         self.comboBox_digit_GASPS.currentIndex()),
-        #                                     ):
+        # # проверяю чекбокс "с преступлениями" и другими условиями
+        # if self.checkBox_prest_IC.checkState() == 2 and\
+        #         (self.comboBox_liter_prest_IC.itemText(self.comboBox_liter_prest_IC.currentIndex()) not in
+        #          (self.text_empty_combobox, self.comboBox_liter_IC.itemText(
+        #              self.comboBox_liter_IC.currentIndex()))):
+        #     self.flag_edit_prest = True
         #
-        #     # проверяю чекбокс "с преступлениями" и другими условиями
-        #     if self.checkBox_prest_IC.checkState() == 2 and\
-        #             (self.comboBox_liter_prest_IC.itemText(self.comboBox_liter_prest_IC.currentIndex()) not in
-        #              (self.text_empty_combobox, self.comboBox_liter_IC.itemText(
-        #                  self.comboBox_liter_IC.currentIndex()))):
-        #         self.flag_edit_prest = True
-        #
-        #     elif (self.checkBox_prest_IC.checkState() == 2) and\
-        #             (self.comboBox_liter_prest_IC.itemText(
-        #                 self.comboBox_liter_prest_IC.currentIndex()) == self.text_empty_combobox):
-        #         # информационное окно о предупреждении выбора полей
-        #         self.window_select = PyQt5.QtWidgets.QMessageBox()
-        #         self.window_select.setWindowTitle('Поля')
-        #         self.window_select.setText(f'Выберите пустые поля или уберите галочку "с преступлениями"')
-        #         self.window_select.exec_()
-        #         self.flag_edit_prest = False
-        #
-        #     elif (self.checkBox_prest_IC.checkState() == 2) and\
-        #             (self.comboBox_liter_prest_IC.itemText(self.comboBox_liter_prest_IC.currentIndex()) ==
-        #              self.comboBox_liter_IC.itemText(self.comboBox_liter_IC.currentIndex())):
-        #         # информационное окно о сравнении значений двух комбо
-        #         self.window_select = PyQt5.QtWidgets.QMessageBox()
-        #         self.window_select.setWindowTitle('Сравнение')
-        #         self.window_select.setText(f'Поля в строке ИЦ не должны совпадать')
-        #         self.window_select.exec_()
-        #         self.flag_edit_prest = False
-        #     else:
-        #         self.flag_edit_prest = False
-        #
-        #     # формируются диапазоны для обработки данных в файлах из комбобоксов
-        #     range_file_IC = self.comboBox_liter_IC.itemText(self.comboBox_liter_IC.currentIndex()) +\
-        #                     self.comboBox_digit_IC.itemText(self.comboBox_digit_IC.currentIndex()) +\
-        #                     ':' +\
-        #                     self.comboBox_liter_IC.itemText(self.comboBox_liter_IC.currentIndex()) +\
-        #                     self.comboBox_digit_IC.itemText(self.comboBox_digit_IC.count()-1)
-        #
-        #     range_file_GASPS = self.comboBox_liter_GASPS.itemText(self.comboBox_liter_GASPS.currentIndex()) +\
-        #                        self.comboBox_digit_GASPS.itemText(self.comboBox_digit_GASPS.currentIndex()) +\
-        #                        ':' +\
-        #                        self.comboBox_liter_GASPS.itemText(self.comboBox_liter_GASPS.currentIndex()) +\
-        #                        self.comboBox_digit_GASPS.itemText(self.comboBox_digit_GASPS.count()-1)
-        #
-        #     if self.flag_edit_prest:
-        #         # формируется диапазон для обработки колонки преступлений
-        #         range_file_IC_prest = self.comboBox_liter_prest_IC.itemText(
-        #             self.comboBox_liter_prest_IC.currentIndex()) + \
-        #                               self.comboBox_digit_IC.itemText(self.comboBox_digit_IC.currentIndex()) + \
-        #                               ':' + \
-        #                               self.comboBox_liter_prest_IC.itemText(
-        #                                   self.comboBox_liter_prest_IC.currentIndex()) + \
-        #                               self.comboBox_digit_IC.itemText(self.comboBox_digit_IC.count() - 1)
-        #         wb_IC_cells_range_prest = self.wb_file_IC_s[range_file_IC_prest]
-        #
-        #     # сформированные диапазоны из выбранных комбобоксов
-        #     wb_IC_cells_range = self.wb_file_IC_s[range_file_IC]
-        #     wb_GASPS_cells_range = self.wb_file_GASPS_s[range_file_GASPS]
-        #
-        #     if (self.checkBox_prest_IC.checkState() == 0) or\
-        #             (self.checkBox_prest_IC.checkState() == 2 and self.flag_edit_prest):
-        #         # формирование множества из обработанных значений ячеек GASPS
-        #         for row_in_range_GASPS in wb_GASPS_cells_range:
-        #             for cell_in_row_GASPS in row_in_range_GASPS:
-        #                 indexR_GASPS = wb_GASPS_cells_range.index(row_in_range_GASPS)
-        #                 indexC_GASPS = row_in_range_GASPS.index(cell_in_row_GASPS)
-        #
-        #                 if wb_GASPS_cells_range[indexR_GASPS][indexC_GASPS].value == None:
-        #                     wb_GASPS_cell_value = 'None'
-        #                 else:
-        #                     wb_GASPS_cell_value = str(wb_GASPS_cells_range[indexR_GASPS][indexC_GASPS].value)
-        #
-        #                 for ikud in wb_GASPS_cell_value.split(";"):
-        #                     set_data_GASPS.add(ikud.strip().replace('.', ''))
-        #
-        #                 tuple_data_GASPS = tuple(set_data_GASPS)
-        #
-        #         # обработка файла ИЦ
-        #         for row_in_range_IC in wb_IC_cells_range:
-        #             for cell_in_row_IC in row_in_range_IC:
-        #                 # определение адреса ячейки из области данных
-        #                 indexR_IC = wb_IC_cells_range.index(row_in_range_IC)
-        #                 indexC_IC = row_in_range_IC.index(cell_in_row_IC)
-        #
-        #                 # получение координаты и значения ячейки IC
-        #                 if wb_IC_cells_range[indexR_IC][indexC_IC].value == None:
-        #                     wb_IC_cell_value = 'None'
-        #                 else:
-        #                     wb_IC_cell_value = str(wb_IC_cells_range[indexR_IC][indexC_IC].value)
-        #
-        #                 # очистка множества для номеров дел из колонки и
-        #                 # разбивка строки на несколько номеров дел если есть ";"
-        #                 set_data_IC.clear()
-        #                 for ikud in wb_IC_cell_value.split(";"):
-        #                     set_data_IC.add(ikud.strip().replace('.', ''))
-        #
-        #                 tuple_data_IC = tuple(set_data_IC)
-        #
-        #                 # раскраска колонок УД в ИЦ файле
-        #                 for ikud in wb_IC_cell_value.split(";"):
-        #                     ikud_split = ikud.strip().replace('.', '').replace(' ', '')
-        #
-        #                     if (ikud_split in tuple_data_GASPS) and (ikud_split in tuple_data_IC):
-        #                         wb_IC_cells_range[indexR_IC][indexC_IC].fill =\
-        #                             openpyxl.styles.PatternFill(start_color='FF0000', end_color='FF0000',
-        #                                                         fill_type='solid')
-        #                     elif ikud_split not in tuple_data_GASPS:
-        #                         wb_IC_cells_range[indexR_IC][indexC_IC].fill =\
-        #                             openpyxl.styles.PatternFill(start_color='878787', end_color='878787',
-        #                                                         fill_type='solid')
-        #
-        #                     # обработка колонки преступности - добавляется номер УД к номеру преступления
-        #                     if self.flag_edit_prest:
-        #                         wb_IC_cells_range_prest[indexR_IC][indexC_IC].value =\
-        #                             ikud_split + wb_IC_cells_range_prest[indexR_IC][indexC_IC].value
-        #
-        #         # сохраняю файл и закрываю оба
-        #         self.wb_file_IC.save(self.file_IC)
-        #         self.wb_file_IC.close()
-        #         self.wb_file_GASPS.close()
-        #
-        #         # считаю время заполнения
-        #         time_finish = time.time()
-        #         '\n' + '.' * 30 + 'закончено за', round(time_finish - time_start, 1), 'секунд'
-        #
-        #         # информационное окно о сохранении файлов
-        #         self.window_info = PyQt5.QtWidgets.QMessageBox()
-        #         self.window_info.setWindowTitle('Файлы')
-        #         self.window_info.setText(f'Файлы сохранены и закрыты.\n{self.file_IC}\n'
-        #                                  f'Заполнение сделано за {round(time_finish - time_start, 1)} секунд')
-        #         self.window_info.exec_()
-        #
-        #         # очистка переменных от повторного использования
-        #         del set_data_IC
-        #         del set_data_GASPS
-        #         self.flag_edit_prest = None
-        #
-        # else:
+        # elif (self.checkBox_prest_IC.checkState() == 2) and\
+        #         (self.comboBox_liter_prest_IC.itemText(
+        #             self.comboBox_liter_prest_IC.currentIndex()) == self.text_empty_combobox):
         #     # информационное окно о предупреждении выбора полей
         #     self.window_select = PyQt5.QtWidgets.QMessageBox()
         #     self.window_select.setWindowTitle('Поля')
-        #     self.window_select.setText(f'Выберите все поля')
+        #     self.window_select.setText(f'Выберите пустые поля или уберите галочку "с преступлениями"')
         #     self.window_select.exec_()
+        #     self.flag_edit_prest = False
+        #
+        # elif (self.checkBox_prest_IC.checkState() == 2) and\
+        #         (self.comboBox_liter_prest_IC.itemText(self.comboBox_liter_prest_IC.currentIndex()) ==
+        #          self.comboBox_liter_IC.itemText(self.comboBox_liter_IC.currentIndex())):
+        #     # информационное окно о сравнении значений двух комбо
+        #     self.window_select = PyQt5.QtWidgets.QMessageBox()
+        #     self.window_select.setWindowTitle('Сравнение')
+        #     self.window_select.setText(f'Поля в строке ИЦ не должны совпадать')
+        #     self.window_select.exec_()
+        #     self.flag_edit_prest = False
+        # else:
+        #     self.flag_edit_prest = False
+        #
+        # # формируются диапазоны для обработки данных в файлах из комбобоксов
+        # range_file_IC = self.comboBox_liter_IC.itemText(self.comboBox_liter_IC.currentIndex()) +\
+        #                 self.comboBox_digit_IC.itemText(self.comboBox_digit_IC.currentIndex()) +\
+        #                 ':' +\
+        #                 self.comboBox_liter_IC.itemText(self.comboBox_liter_IC.currentIndex()) +\
+        #                 self.comboBox_digit_IC.itemText(self.comboBox_digit_IC.count()-1)
+        #
+        # range_file_GASPS = self.comboBox_liter_GASPS.itemText(self.comboBox_liter_GASPS.currentIndex()) +\
+        #                    self.comboBox_digit_GASPS.itemText(self.comboBox_digit_GASPS.currentIndex()) +\
+        #                    ':' +\
+        #                    self.comboBox_liter_GASPS.itemText(self.comboBox_liter_GASPS.currentIndex()) +\
+        #                    self.comboBox_digit_GASPS.itemText(self.comboBox_digit_GASPS.count()-1)
+        #
+        # if self.flag_edit_prest:
+        #     # формируется диапазон для обработки колонки преступлений
+        #     range_file_IC_prest = self.comboBox_liter_prest_IC.itemText(
+        #         self.comboBox_liter_prest_IC.currentIndex()) + \
+        #                           self.comboBox_digit_IC.itemText(self.comboBox_digit_IC.currentIndex()) + \
+        #                           ':' + \
+        #                           self.comboBox_liter_prest_IC.itemText(
+        #                               self.comboBox_liter_prest_IC.currentIndex()) + \
+        #                           self.comboBox_digit_IC.itemText(self.comboBox_digit_IC.count() - 1)
+        #     wb_IC_cells_range_prest = self.wb_file_IC_s[range_file_IC_prest]
+        #
+        # # сформированные диапазоны из выбранных комбобоксов
+        # wb_IC_cells_range = self.wb_file_IC_s[range_file_IC]
+        # wb_GASPS_cells_range = self.wb_file_GASPS_s[range_file_GASPS]
+        #
+        # if (self.checkBox_prest_IC.checkState() == 0) or\
+        #         (self.checkBox_prest_IC.checkState() == 2 and self.flag_edit_prest):
+        #     # формирование множества из обработанных значений ячеек GASPS
+        #     for row_in_range_GASPS in wb_GASPS_cells_range:
+        #         for cell_in_row_GASPS in row_in_range_GASPS:
+        #             indexR_GASPS = wb_GASPS_cells_range.index(row_in_range_GASPS)
+        #             indexC_GASPS = row_in_range_GASPS.index(cell_in_row_GASPS)
+        #
+        #             if wb_GASPS_cells_range[indexR_GASPS][indexC_GASPS].value == None:
+        #                 wb_GASPS_cell_value = 'None'
+        #             else:
+        #                 wb_GASPS_cell_value = str(wb_GASPS_cells_range[indexR_GASPS][indexC_GASPS].value)
+        #
+        #             for ikud in wb_GASPS_cell_value.split(";"):
+        #                 set_data_GASPS.add(ikud.strip().replace('.', ''))
+        #
+        #             tuple_data_GASPS = tuple(set_data_GASPS)
+        #
+        #     # обработка файла ИЦ
+        #     for row_in_range_IC in wb_IC_cells_range:
+        #         for cell_in_row_IC in row_in_range_IC:
+        #             # определение адреса ячейки из области данных
+        #             indexR_IC = wb_IC_cells_range.index(row_in_range_IC)
+        #             indexC_IC = row_in_range_IC.index(cell_in_row_IC)
+        #
+        #             # получение координаты и значения ячейки IC
+        #             if wb_IC_cells_range[indexR_IC][indexC_IC].value == None:
+        #                 wb_IC_cell_value = 'None'
+        #             else:
+        #                 wb_IC_cell_value = str(wb_IC_cells_range[indexR_IC][indexC_IC].value)
+        #
+        #             # очистка множества для номеров дел из колонки и
+        #             # разбивка строки на несколько номеров дел если есть ";"
+        #             set_data_IC.clear()
+        #             for ikud in wb_IC_cell_value.split(";"):
+        #                 set_data_IC.add(ikud.strip().replace('.', ''))
+        #
+        #             tuple_data_IC = tuple(set_data_IC)
+        #
+        #             # раскраска колонок УД в ИЦ файле
+        #             for ikud in wb_IC_cell_value.split(";"):
+        #                 ikud_split = ikud.strip().replace('.', '').replace(' ', '')
+        #
+        #                 if (ikud_split in tuple_data_GASPS) and (ikud_split in tuple_data_IC):
+        #                     wb_IC_cells_range[indexR_IC][indexC_IC].fill =\
+        #                         openpyxl.styles.PatternFill(start_color='FF0000', end_color='FF0000',
+        #                                                     fill_type='solid')
+        #                 elif ikud_split not in tuple_data_GASPS:
+        #                     wb_IC_cells_range[indexR_IC][indexC_IC].fill =\
+        #                         openpyxl.styles.PatternFill(start_color='878787', end_color='878787',
+        #                                                     fill_type='solid')
+        #
+        #                 # обработка колонки преступности - добавляется номер УД к номеру преступления
+        #                 if self.flag_edit_prest:
+        #                     wb_IC_cells_range_prest[indexR_IC][indexC_IC].value =\
+        #                         ikud_split + wb_IC_cells_range_prest[indexR_IC][indexC_IC].value
+        #
+        #     # сохраняю файл и закрываю оба
+        #     self.wb_file_IC.save(self.file_IC)
+        #     self.wb_file_IC.close()
+        #     self.wb_file_GASPS.close()
+        #
+        #     # считаю время заполнения
+        #     time_finish = time.time()
+        #     '\n' + '.' * 30 + 'закончено за', round(time_finish - time_start, 1), 'секунд'
+        #
+        #     # информационное окно о сохранении файлов
+        #     self.window_info = PyQt5.QtWidgets.QMessageBox()
+        #     self.window_info.setWindowTitle('Файлы')
+        #     self.window_info.setText(f'Файлы сохранены и закрыты.\n{self.file_IC}\n'
+        #                              f'Заполнение сделано за {round(time_finish - time_start, 1)} секунд')
+        #     self.window_info.exec_()
+        #
+        #     # очистка переменных от повторного использования
+        #     del set_data_IC
+        #     del set_data_GASPS
+        #     self.flag_edit_prest = None
 
     # событие - нажатие на кнопку Выход
     def click_on_btn_exit(self):
